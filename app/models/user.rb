13c954e5db29727e76d39c5b127d13ed8ca04e41
EC2 Default User <ec2-user@ip-172-31-6-235.ap-northeast-1.compute.internal>
2022-12-05 22:17:15 +0000

    model doing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
   has_many :books, dependent: :destroy
   validates :name, uniqueness: true, length: {minimum:2, maximum:20}
   
+  
+  def get_profile_image(size)
+    unless profile_image.attached?
+      file_path = Rails.root.join
 end
